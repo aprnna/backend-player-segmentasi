6f443f489b7519b0e3477a9baa8f567b2c0fb3c6
aprnna <121468657+aprnna@users.noreply.github.com>
2025-08-15 19:56:58 +0700

    connect db

--- src/config/config.py
+++ src/config/config.py
@@ -6,10 +6,21 @@ PORT = env["PORT"] or 5000
 DEBUG = env["DEBUG"] or True
 GROQ_API_KEY = env["GROQ_API_KEY"] or "your_groq_api_key_here"
 
-DATABASE_URL = env["DATABASE_URL"] or "mysql://root:root@localhost:3306/se_ticket"
+# DATABASE_URL = env["DATABASE_URL"] or "mysql://root:root@localhost:3306/se_ticket"
 
 JWT_ACCESS_TOKEN_EXPIRES = env["JWT_ACCESS_TOKEN_EXPIRES"] or 60 * 60 * 24 * 7
 JWT_ACCESS_TOKEN_SECRET = env["JWT_ACCESS_TOKEN_SECRET"] or "secret"
 JWT_ACCESS_TOKEN_ALGORITHM = env["JWT_ACCESS_TOKEN_ALGORITHM"] or "HS256"
 
+# SSH Tunnel Config
+SSH_HOST = "104.214.168.165"
+SSH_PORT = 22
+SSH_USERNAME = "nuruldinikamilah"
+SSH_PASSWORD = "Nurulsnaily123"
+
+
+# MySQL Config
+MYSQL_USER = "root"
+MYSQL_PASSWORD = "password"
+MYSQL_DB = "player_segmentasi"
 
--- src/config/database.py
+++ src/config/database.py
@@ -1,14 +1,32 @@
 from flask_sqlalchemy import SQLAlchemy
-from src.config.config import DATABASE_URL
+from src.config.config import SSH_HOST, SSH_PORT, SSH_USERNAME, SSH_PASSWORD, MYSQL_DB, MYSQL_PASSWORD, MYSQL_USER
+from sshtunnel import SSHTunnelForwarder
+
+server = None  # tunnel global
+
+def start_ssh_tunnel():
+    global server
+    server = SSHTunnelForwarder(
+        (SSH_HOST, SSH_PORT),
+        ssh_username=SSH_USERNAME,
+        ssh_password=SSH_PASSWORD,  # bisa diganti key_file kalau pakai private key
+        remote_bind_address=('127.0.0.1', 3306),
+        local_bind_address=('127.0.0.1', 3307)
+    )
+    server.start()
+    print(f"[INFO] SSH Tunnel aktif di port lokal {server.local_bind_port}")
+
 
 def database(app):
-  global db
+  global db, server
   if 'db' not in globals():
-    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+    start_ssh_tunnel()
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@127.0.0.1:{server.local_bind_port}/{MYSQL_DB}"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db = SQLAlchemy(app)
     print("[INFO] Database connected")
   return db
+
 def generateDatabase(main_app,db):
   with main_app.app_context():
     db.create_all()
